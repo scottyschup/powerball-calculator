eccd5fd4433e0ad7f6d207ca549ecc23aeb18422
Scott Schupbach <sschupbach@asperasoft.com>
2016-01-12 18:31:41 -0800

    Add shell interface

---
+++ README.md
@@ -0,0 +1,5 @@
+# Powerball Calculator
+
+With the Powerball jackpot reaching a record-breaking $1.5B, you may be tempted to take up a career in playing the lottery. I'm here to help you realize how short a career this would be.
+
+Clone the repo and cd into it. Run `$ ruby pbcalc.rb` and then follow the prompts.
--- pbcalc.rb
+++ pbcalc.rb
@@ -1,4 +1,6 @@
 class PowerballCalculator
+  attr_reader :num_combos
+
   PAYOUTS = {
     '0+': { value: 4, odds: 38.32 },
     '1+': { value: 4, odds: 91.98 },
@@ -87,5 +89,33 @@ class PowerballCalculator
     end
 
     total
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  title = "Powerball Calculator"
+  puts "#{title}\n#{'=' * title.length}"
+  puts "How much is the next jackpot (in millions)?"
+  puts "Ex: for $650,000,000 type '650'. For $1.5B put '1500'.\n"
+  jackpot = gets.chomp.to_i * 1_000_000
+  calc = PowerballCalculator.new(jackpot)
+
+  puts "Total winnings: #{calc.num_combos * calc.average_ticket_value}"
+  puts "Ticket combinations: #{calc.num_combos}"
+  puts "Average ticket value: #{calc.average_ticket_value}\n\n"
+
+  puts "How many tickets would you like to buy?\n"
+  num_tix = gets.chomp.to_i
+  results = calc.simulator(num_tix)
+  puts "Out of #{results[:attempts]} attempts, you won #{results[:wins]} times."
+  puts "Your biggest winning ticket is worth $#{results[:max_win]}."
+  puts "Your total winnings are $#{results[:winnings]}."
+  outcome = results[:outcome] > 0 ? '$' : '-$'
+  outcome += results[:outcome].abs.to_s
+  puts "At $2 per ticket, your net profit is #{outcome}."
+  if results[:outcome] > 0
+    puts "Congrats! You beat the odds!"
+  else
+    puts "Better luck next time :("
   end
 end
